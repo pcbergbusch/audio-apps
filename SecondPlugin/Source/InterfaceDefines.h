3b759fe01ec5ee560938f99a08975d7cc3734c38
Paul Bergbusch <pcbergbusch@gmail.com>
2022-12-21 17:57:53 +0100

    add colours and fonts

--- SecondPlugin/Source/InterfaceDefines.h
+++ SecondPlugin/Source/InterfaceDefines.h
@@ -8,6 +8,8 @@
   ==============================================================================
 */
 
+#include "JuceHeader.h"
+
 #pragma once
 
 #define MAIN_PANEL_WIDTH                750
@@ -27,3 +29,15 @@
 
 #define FX_PANEL_WIDTH                  CENTER_PANEL_WIDTH
 #define FX_PANEL_HEIGHT                 CENTER_PANEL_HEIGHT - CENTER_PANEL_MENU_BAR_HEIGHT
+
+const juce::Colour colourSteel = juce::Colour(125, 125, 125);
+const juce::Colour colourGray = juce::Colour(105, 105, 105);
+const juce::Colour colourGrayTrans70 = juce::Colour(105, 105, 105).withAlpha(0.3f);
+const juce::Colour colourBlackTrans100 = juce::Colour(0, 0, 0).withAlpha(0.0f);
+const juce::Colour colourBlackTrans70 = juce::Colour(0, 0, 0).withAlpha(0.3f);
+const juce::Colour colourBlackTrans40 = juce::Colour(0, 0, 0).withAlpha(0.6f);
+const juce::Colour colourBlackTrans20 = juce::Colour(0, 0, 0).withAlpha(0.8f);
+
+const juce::Font fontHelv12Bold{ "Helvetica Neue", 12.00f, juce::Font::bold };
+const juce::Font fontHelv22Bold{ "Helvetica Neue", 22.00f, juce::Font::bold };
+const juce::Font fontHelv48Bold{ "Helvetica Neue", 48.00f, juce::Font::bold };
