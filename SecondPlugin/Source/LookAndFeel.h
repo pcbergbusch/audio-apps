4da17f0728d1797ddef685183aa0f020ebbb3c72
Paul Bergbusch <pcbergbusch@gmail.com>
2022-12-22 18:27:02 +0100

    add methods to LookAndFeel.h

--- SecondPlugin/Source/LookAndFeel.h
+++ SecondPlugin/Source/LookAndFeel.h
@@ -9,3 +9,26 @@
 */
 
 #pragma once
+
+#include "JuceHeader.h"
+#include "InterfaceDefines.h"
+
+class LookAndFeel
+    :public juce::LookAndFeel_V4
+{
+public:
+    LookAndFeel();
+    ~LookAndFeel();
+
+    juce::Font getTextButtonFont(juce::TextButton&, int buttonHeight) override
+    {
+        return fontHelv12Bold;
+    }
+
+    void drawButtonBackground(Graphics&, Button&, const Colour& backgroundColour,
+        bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    {
+        juce::Colour fillColour;
+    }
+
+};
